2741a0197c54610e61adc8abdae0848b9e133bd9
James-koduah <jameskoduah171@gmail.com>
2023-05-30 21:07:10 +0000

    up

--- app/populate.js
+++ app/populate.js
@@ -22,48 +22,38 @@ let divs = {
         ]
     }
 }
-let work_section = document.getElementById('works')
-for (let elem in divs){
-    let main_body = document.createElement('div')
-    main_body.classList.add('work_container')
-    main_body.id = `${elem}`
-    let work_text = document.createElement('div')
-    work_text.classList.add('work_text')
-    let work_text_h3 = document.createElement('h3')
-    work_text_h3.innerHTML = `${divs[elem]["name"]}`
-    let grid_pics = document.createElement('div')
-    grid_pics.classList.add('grid_pics')
-    let pictures = divs[elem]["displayPictures"]
-    for (let pics of pictures){
-        let pic_div = document.createElement('div')
-        pic_div.classList.add('pics')
-        if (pics[1] == 1) pic_div.classList.add('long_pic')
-        if (pics[1] == 2) pic_div.classList.add('wide_pic')
-        let pic_div_img = document.createElement('img')
-        pic_div_img.src = divs[elem]["path"] + pics[0] + '.jpg'
-        pic_div_img.setAttribute('id', `${elem}_${pics[0]}`)
-        pic_div_img.classList.add('work_grid_img')
-        pic_div_img.setAttribute('onclick', "full_display(this)")
-        pic_div.appendChild(pic_div_img)
-        grid_pics.appendChild(pic_div)
+function populate(){
+    let work_section = document.getElementById('works');
+    for (let elem in divs){
+        let main_body = document.createElement('div');
+        let work_text = document.createElement('div');
+        let work_text_h3 = document.createElement('h3');
+        let grid_pics = document.createElement('div');
+        main_body.classList.add('work_container');
+        main_body.id = `${elem}`;
+        work_text.classList.add('work_text');
+        work_text_h3.innerHTML = `${divs[elem]["name"]}`;
+        grid_pics.classList.add('grid_pics');
+        let pictures = divs[elem]["displayPictures"];
+        for (let pics of pictures){
+            let pic_div = document.createElement('div');
+            let pic_div_img = document.createElement('img');
+            pic_div.classList.add('pics');
+            if (pics[1] == 1) pic_div.classList.add('long_pic');
+            if (pics[1] == 2) pic_div.classList.add('wide_pic');
+            pic_div_img.src = divs[elem]["path"] + pics[0] + '.jpg';
+            pic_div_img.setAttribute('id', `${elem}_${pics[0]}`);
+            pic_div_img.classList.add('work_grid_img');
+            pic_div_img.setAttribute('onclick', "full_display(this)");
+            pic_div.appendChild(pic_div_img);
+            grid_pics.appendChild(pic_div);
+        }
+        
+        main_body.appendChild(grid_pics);
+        work_text.appendChild(work_text_h3);
+        main_body.appendChild(work_text);
+        work_section.appendChild(main_body);
     }
-    
-    main_body.appendChild(grid_pics)
-    work_text.appendChild(work_text_h3)
-    main_body.appendChild(work_text)
-    work_section.appendChild(main_body)
 }
 
- /**
- *  let img = document.createElement('img');
-      img.id = 'imgId';
-      img.src = '/uploads/media/default/0001/05/e9f3899d915c17845be51e839d5ba238f0404b07.png';
-      document.body.appendChild(img);
-      img.addEventListener("click", imgSize);
-      function imgSize() {
-        let myImg = document.querySelector("#imgId");
-        let realWidth = myImg.naturalWidth;
-        let realHeight = myImg.naturalHeight;
-        alert("Original width=" + realWidth + ", " + "Original height=" + realHeight);
-      }
- */
+populate()
