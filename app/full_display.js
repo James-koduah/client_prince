891240074096ca229f70533592d506efb2b90deb
James-koduah <jameskoduah171@gmail.com>
2023-06-14 21:22:29 +0000

    updated the promotions, first pitch section

--- app/full_display.js
+++ app/full_display.js
@@ -1,43 +1,51 @@
+/**
+ * Add the full display functionality to all <img> elements
+ */
 let images = document.querySelectorAll('img');
   for (let i = 0; i < images.length; i++){
     images[i].setAttribute('onclick', 'full_display(this)')
   }
-  /**
-   * Full Display section
-   */
-  
-  let full_display_running = false;
-  let full_display_section = document.getElementById('full_display')
-  let full_display_img = document.getElementById('full_display_img')
-  /**
-   * Close the Full Display
-   */
-  function full_display_close(){
-    full_display_section.style.zIndex = -40;
-    full_display_running = false
-  }
 
 
-  /**
-   * Open the Full Display
-   */
-  function full_display(image){
-    image_source = image.src
-    full_display_img.src = image_source
-    full_display_section.style.zIndex = 40
-    full_display_running = true
-  }
+/**
+ * Full Display section
+ */
+let full_display_running = false;
+let full_display_section = document.getElementById('full_display')
+let full_display_img = document.getElementById('full_display_img')
+/**
+ * Close the Full Display
+ */
+function full_display_close(){
+full_display_section.style.zIndex = -40;
+full_display_running = false
+}
 
 
+/**
+ * Open the Full Display
+ */
+function full_display(image){
+image_source = image.src
+full_display_img.src = image_source
+full_display_section.style.zIndex = 40
+full_display_running = true
+}
 
-  jQuery(document).ready(function($) {
-    if (window.history && window.history.pushState) {
-      window.history.pushState('forward', null, './#forward');
-      $(window).on('popstate', function() {
+
+/**
+ * Handle the event where the user presses the device backbutton 
+ * instead of the |X| at the top of the page
+ */
+jQuery(document).ready(function($) {
+if (window.history && window.history.pushState) {
+    window.history.pushState('forward', null, './#forward');
+    $(window).on('popstate', function() {
         if (full_display_running == true){
             full_display_close()
+            if (screen.width < 800) alert('Please Use the | X |')
         }
-      });
-  
-    }
-  });
+    });
+
+}
+});
--- app/main.js
+++ app/main.js
@@ -37,7 +37,7 @@ function change_slide(current){
  * populate the first_display section
 */
 let first_display_pics = document.getElementsByClassName('picture_first_work')
-let pic_num = 6;
+let pic_num = 0;
 for (let i = 0; i < first_display_pics.length; i++){
     let img = document.createElement('img')
     img.src = `resources/stonedesign/stonedesign_${pic_num}.webp`
