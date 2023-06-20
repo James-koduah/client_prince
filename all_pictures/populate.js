812afc436746d89ff0f6b7b262c5b71dca0cab89
James-koduah <jameskoduah171@gmail.com>
2023-06-20 20:17:36 +0000

    Made the Menu bar; some improvements;

--- all_pictures/populate.js
+++ all_pictures/populate.js
@@ -1,10 +1,21 @@
 let main = document.getElementById('main')
 let main_divs = document.getElementsByClassName('main_div')
 let div_num = 0
-pic_num = 0;
+let pic_num = 0;
 for (let i = 0; i < 32; i++){
     let img = document.createElement('img')
     img.src = `../resources/stonedesign/stonedesign_${pic_num}.webp`
+    img.setAttribute('loading', 'lazy')
+    main_divs[div_num].appendChild(img)
+    div_num++
+    if (div_num == main_divs.length) div_num = 0
+    pic_num++
+}
+
+pic_num = 0;
+for (let i = 0; i < 190; i++){
+    let img = document.createElement('img')
+    img.src = `../resources/unsifted/all(${pic_num}).jpg`
     img.setAttribute('loading', 'lazy')
     main_divs[div_num].appendChild(img)
     div_num++
--- app/main.js
+++ app/main.js
@@ -1,4 +1,9 @@
-
+let flicker = document.querySelector('#flicker')
+function tumble(){
+    flicker.className += ' flicker'
+    setTimeout(()=>{flicker.className = ''},1000)
+    
+}
 
 /**
  * This block of code is for the slide_show on the welcome page
@@ -66,5 +71,60 @@ window.onscroll = function (e) {
     }
   };
 
+/**
+   * On Mobile devices or smaller screen widths
+   * nav_toogle - To toogle the navigation bar with a click of the menu button
+   * @nav_toggle_on: The switch to tell if the navigation bar is hidden or displayed
+   * Return: Nothing
+*/
+let menu_bar = document.getElementById("menu_bar");
+let nav_menu_button = document.getElementById("nav_menu_button");
 
+let nav_toggle_on = false;
+function nav_toogle() {
+  if (nav_toggle_on == false) {
+    menu_bar.style.zIndex = 20;
+    nav_toggle_on = true;
+  }
+  else if (nav_toggle_on == true) {
+    menu_bar.style.zIndex = -39;
+    nav_toggle_on = false;
+  }
+}
+
+/**
+ * Light and Dark Mode
+ */
+let dark = true
+function light_dark(){
+  if (dark == true) {
+    document.documentElement.style.setProperty('--main-bg-color', '#fff')
+    document.documentElement.style.setProperty('--soft-bg-color', '#f4f4f4')
+    document.documentElement.style.setProperty('--main-bg-color-faded', '#f9f6f0b3')
+    document.documentElement.style.setProperty('--minor-bg-color', '#f6ddf4')
+    document.documentElement.style.setProperty('--opposite-bg-color', '#545456')
+    document.documentElement.style.setProperty('--text-color', '#2b2a2a')
+    document.documentElement.style.setProperty('--color-border', '#686868')
+    document.documentElement.style.setProperty('--extreme-text-color', '#000000')
+    document.documentElement.style.setProperty('--about-text-color', '#4f4c50')
+    document.documentElement.style.setProperty('--main-menu', '#c0c0c0')
+    document.documentElement.style.setProperty('--main-menu-div', '#626161')
+    dark = false
+  }
+  else
+  if (dark == false){
+    document.documentElement.style.setProperty('--main-bg-color', '#000000')
+    document.documentElement.style.setProperty('--soft-bg-color', '#212020')
+    document.documentElement.style.setProperty('--main-bg-color-faded', '#1f1f1fb3')
+    document.documentElement.style.setProperty('--minor-bg-color', '#333133')
+    document.documentElement.style.setProperty('--opposite-bg-color', '#f9f6f0')
+    document.documentElement.style.setProperty('--text-color', '#cac6c6')
+    document.documentElement.style.setProperty('--color-border', '#f7f4ed')
+    document.documentElement.style.setProperty('--extreme-text-color', '#ffffff')
+    document.documentElement.style.setProperty('--about-text-color', '#bebcbf')
+    document.documentElement.style.setProperty('--main-menu', '#626161')
+    document.documentElement.style.setProperty('--main-menu-div', '#c0c0c0')
+    dark = true
+  }
+}
   
